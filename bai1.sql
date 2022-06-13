606239651d25fa4206d008311b4715da14599e01
Kisibian108 <102302311+Kisibian108@users.noreply.github.com>
2022-06-13 17:18:46 +0700

    bai1

---
+++ bai1.sql
@@ -0,0 +1,11 @@
+create table class(
+id int primary key auto_increment,
+`name` varchar(45) 
+);
+
+create table teacher(
+id int primary key auto_increment,
+`name` varchar(45),
+age int,
+country varchar(45)
+);
